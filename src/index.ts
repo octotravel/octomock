e44616cf7d19336795db14317231737d30b037dd
Aleš Fabiánek <fabianek.ales@gmail.com>
2023-01-18 10:47:58 +0100

    Added dotenv

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,9 @@ import { ValidationError } from "yup";
 import {
   OctoError,
   InternalServerError,
-  BadRequestError
+  BadRequestError,
 } from "./models/Error";
+import "dotenv/config";
 
 const app = new Koa();
 
@@ -36,7 +37,7 @@ app.use(async (ctx, next) => {
       ctx.status = error.status;
       ctx.body = {
         ...error.body,
-        stack: error.stack
+        stack: error.stack,
       };
     }
   }
@@ -48,4 +49,4 @@ app.use(async (ctx, next) => {
 app.use(parseCapabilities);
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(process.env.APP_PORT ?? 3000);
